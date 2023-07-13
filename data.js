3ea9812b4c1fb323872a90544ba62c7853ee6b7e
Anamika Soni <94427524+Anamikasoni31@users.noreply.github.com>
2023-07-13 17:02:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics-20230713T061234Z-001/musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images-20230713T061151Z-001/images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'musics-20230713T061234Z-001/musics/Song 1.mp3',
+        artist: 'artist 2',
+        cover: 'images-20230713T061151Z-001/images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics-20230713T061234Z-001/musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images-20230713T061151Z-001/images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'musics-20230713T061234Z-001/musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images-20230713T061151Z-001/images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'musics-20230713T061234Z-001/musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images-20230713T061151Z-001/images/cover 5.png'
+    },
+]
